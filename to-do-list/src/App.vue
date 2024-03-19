eb0d7a2c323e872d413a4569dbee0a055051a41e
ekahanny <ekahnny@gmail.com>
2024-03-19 14:42:49 +0800

    Create form component

--- to-do-list/src/App.vue
+++ to-do-list/src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 class="mt-5">TO DO LIST</h1>
+  <ToDoForm></ToDoForm>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoForm from "./components/ToDoForm.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld,
+    ToDoForm,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+}
+
+h1 {
+  font-family: "Newsreader", serif;
 }
 </style>
---
+++ to-do-list/src/components/ToDoForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <form class="row g-1">
+    <div class="col-9">
+      <input
+        type="text"
+        class="form-control"
+        id="newTask"
+        placeholder="Add New Task..."
+      />
+    </div>
+    <div class="col">
+      <button type="submit" class="btn btn-primary">+</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "ToDoForm",
+};
+</script>
