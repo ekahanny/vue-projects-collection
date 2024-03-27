687c2107af9792082687eda07692df11add16ed1
ekahanny <ekahnny@gmail.com>
2024-03-28 03:38:52 +0800

    menambahkan method

--- random-number/src/App.vue
+++ random-number/src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="counter">
+    <input type="text" class="input-box" placeholder="0" v-model="number" />
+    <button class="start" @click="generateNumber">Take Your Guess!</button>
+  </div>
+  <div class="hasil">
+    <div>Hasil</div>
+    <div class="list">
+      <div class="item">Status: {{ status }}</div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      number: 0,
+      status: "", // Menambahkan properti status
+    };
+  },
+  methods: {
+    generateNumber: function () {
+      let randomNumber = Math.floor(Math.random() * 100);
+      if (this.number > randomNumber) {
+        this.status = "Tebakanmu terlalu besar!"; // Mengatur status tebakan
+      } else if (this.number < randomNumber) {
+        this.status = "Tebakanmu terlalu kecil!"; // Mengatur status tebakan
+      } else if (this.number === randomNumber) {
+        this.status = "Tebakanmu benar!"; // Mengatur status tebakan
+      }
+
+      setTimeout(() => {
+        this.status = "";
+      }, 2000);
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/css/style.css";
 </style>
