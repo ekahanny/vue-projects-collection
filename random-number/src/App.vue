a9d4c41d2ddf1b183937ed587bcac100968e3240
ekahanny <ekahnny@gmail.com>
2024-03-30 01:42:22 +0800

    completed the code

--- random-number/src/App.vue
+++ random-number/src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="counter">
     <input type="text" class="input-box" placeholder="0" v-model="number" />
-    <button class="start" @click="generateNumber">Take Your Guess!</button>
+    <button class="start" @click="generateNumber" :disabled="isCounting">
+      Take Your Guess!
+    </button>
   </div>
   <div class="hasil">
     <div>Hasil</div>
     <div class="list">
-      <div class="item">Hasil : {{ number }}</div>
+      <div class="item" v-for="(num, index) in randomNumber" :key="index">
+        Hasil : {{ num }}
+      </div>
     </div>
   </div>
 </template>
@@ -16,13 +20,30 @@ export default {
   name: "App",
   data() {
     return {
+      isCounting: false,
       number: 0,
+      randomNumber: [],
     };
   },
   methods: {
     generateNumber: function () {
-      let randomNumber = Math.floor(Math.random() * 100);
-      this.number = randomNumber;
+      if (this.isCounting) {
+        return;
+      }
+
+      let counter = 0;
+      this.isCounting = true;
+
+      let instance = setInterval(() => {
+        this.number = Math.floor(Math.random() * 100) + 1;
+        counter++;
+
+        if (counter >= 10) {
+          this.randomNumber.push(this.number);
+          clearInterval(instance);
+          this.isCounting = false;
+        }
+      }, 50);
     },
   },
 };
