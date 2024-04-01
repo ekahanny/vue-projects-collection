20bb9d20e524794c4dc84d8405d607c97ed38f67
ekahanny <ekahnny@gmail.com>
2024-04-01 20:52:36 +0800

    added cardcomponent

--- temperature-converter/src/App.vue
+++ temperature-converter/src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <!-- Image -->
+      <div class="col-md-3">
+        <img
+          alt="temperature"
+          src="./assets/temperature.png"
+          width="550"
+          height="auto"
+          class="img-fluid"
+        />
+      </div>
+
+      <!-- Form Input -->
+      <div class="col-md-9">
+        <div class="card border border-warning">
+          <div class="card-body py-4">
+            <CardComponent />
+          </div>
+        </div>
+
+        <!-- Card "Result" -->
+        <div class="card border border-warning mt-3 w-100">
+          <div class="card-body py-4">
+            <p class="ps-4">Result</p>
+            <p class="ps-4">0 C</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import CardComponent from "./components/CardComponent.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    CardComponent,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ temperature-converter/src/components/CardComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="row d-flex justify-content-center align-items-center">
+    <div class="col-md-5">
+      <div class="form-group">
+        <label for="suhu" class="mb-2">Suhu:</label>
+        <input
+          type="number"
+          class="form-control border border-danger"
+          id="suhu"
+          placeholder="Masukkan suhu"
+        />
+      </div>
+    </div>
+    <div class="col-md-5">
+      <div class="form-group">
+        <label for="tipe" class="mb-2">Tipe:</label>
+        <select class="form-select border border-danger" id="tipe">
+          <option value="" selected disabled>Pilih Satuan Suhu...</option>
+          <option value="celcius">Celcius</option>
+          <option value="fahrenheit">Fahrenheit</option>
+          <option value="kelvin">Kelvin</option>
+        </select>
+      </div>
+    </div>
+    <div class="col-md-auto">
+      <div class="form-group">
+        <label for="submit">&nbsp;</label>
+        <button class="btn btn-danger form-control">Submit</button>
+      </div>
+    </div>
+  </div>
+</template>
